aed06f510e2de5de6e3c379ba4bb668065bf4ae1
Htoo Thar <hsellnayhtoo2001@gmaial.com>
2026-05-25 15:13:54 +0700

    vue another commit

---
+++ about.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+       about page 
+              vue doesn't need to reload the browser
+
+    </div>
+</template>
---
+++ index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="mainhost">
+    <div class="container">
+      <input class="text"
+        type="text"
+        placeholder="username"
+      >
+      <input class="date"
+        type="date"
+        placeholder="date of birth">
+      <button class="btn-submit" @click="handleSubmit">Submit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSubmit() {
+      alert("Thanks for submitting your information!");
+    }
+  }
+}
+</script>
+
+<style>
+  .mainhost {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background: rgb(90, 20, 20);
+  }
+  .text {
+    margin: 15px;
+  }
+  .date {
+    margin:15px;
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; 
+    background: gray;
+    height: 400px;
+    width: 500px;
+  
+  }  
+
+  .btn-submit {
+    background: red;
+    padding: 5px;
+    margin: 15px;
+  }
+</style>
+
+
+
+<!-- <template>
+  <div class="container">
+    <input
+      type="text"
+      placeholder="username"
+    >
+    <button class="btn-submit">Submit</button>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style>
+  .container {
+    /* background: green; */
+  }
+
+  .btn-submit {
+    background: red;
+    padding: 5px;
+  }
+</style> -->
